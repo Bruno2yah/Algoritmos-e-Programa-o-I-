1d29960930009bec4772c66f14f4a11b80da3538
BRUNO GEANINI DOS REIS <bruno.greis1@senacsp.edu.br>
2026-03-04 11:51:10 -0300

    area circulo arrumado

--- exercicio11.java
+++ exercicio11.java
@@ -1,9 +1,8 @@
-import java.lang.Math;
 public class exercicio11 {
     public static void main(String[] args) {
         double area, pi = 3.14159, raio = 5;
 
-        area = Math.pow(pi, raio*raio);
+        area = pi*(raio*raio);
 
         System.out.printf("O valor da Area de um circulo de 5cm é: %.2f", area);
 
